fa9a22fbb82cd3e4825c63b746a776594a73f6f8
Logan <Logan.Gittelson@gmail.com>
2015-03-20 14:59:12 -0400

    More cleanup
    deleted "head" and "orgin/master" stuff

--- Youtube Looper Source/contentscript.js
+++ Youtube Looper Source/contentscript.js
@@ -2094,22 +2094,14 @@ ytl = {
 				ytl.player.addEventListener('onStateChange', ytl.loopAction, false);
 				clearInterval(ytl.doubleChecker);
 				ytl.doubleChecker = setInterval(ytl.onStateChangeCheckAction, 2000);
-<<<<<<< HEAD
-                
-=======
-				
->>>>>>> origin/master
+
                 // L: Added a function to check for seeks every second
                 clearInterval(ytl.seekChecker);
                 ytl.seekChecker = setInterval(ytl.catchSeeks, 1000);
 				// L: Adding my own event listener - may need to be outside this func?
 				ytl.player.removeEventListener('onStateChange', ytl.trackChange, false);
 				ytl.player.addEventListener('onStateChange', ytl.trackChange, false);
-<<<<<<< HEAD
-                
-=======
-				
->>>>>>> origin/master
+
 			} else {
 				ytl.log('NO REFERENCE PLAYER', '(Usually cause by using other youtube extensions at the same time)');
 				return;
